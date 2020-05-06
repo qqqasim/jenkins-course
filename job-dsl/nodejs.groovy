90bc02dbaf0d6c6691ceda6b700367aeb8db2573
qqqasim <61903947+qqqasim@users.noreply.github.com>
2020-05-06 11:00:19 +0500

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git@github.com:qqqasim/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('qasim@zaryansgroup.com')
         }
     }
     triggers {
